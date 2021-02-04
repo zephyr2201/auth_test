2594cdc3384b735c9f53948a2b4e24878afb9395
Ersin <78481146+zephyr2201@users.noreply.github.com>
2021-02-04 16:15:49 +0600

    Update settings.py

--- biometric_test_project-main/core/settings.py
+++ biometric_test_project-main/core/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-export DJANGO_SETTINGS_MODULE=core.settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
